740f655a33da17792c775f71c4d4ca3b9ca2a4c9
Gabriel Souki Hernandez <gabrielsouki@gmail.com>
2024-04-11 12:34:14 -0400

    Added homescreen, players-settings and game sections to HTML. And some testing logic to transition between homescreen and players-settings

--- src/main.mjs
+++ src/main.mjs
@@ -1,1 +1,15 @@
-console.log("Hello world");
+const homeScreenSection = document.querySelector(".homescreen");
+const playerSettingsSection = document.querySelector(".players-settings");
+const gameSection = document.querySelector(".game");
+const newGameButton = document.querySelector(".newgame-button");
+
+newGameButton.addEventListener("click", NewGameClickHandler)
+
+
+
+function NewGameClickHandler()
+{
+    homeScreenSection.classList.add("inactive");
+    playerSettingsSection.classList.remove("inactive");
+    gameSection.classList.add("inactive");
+}
